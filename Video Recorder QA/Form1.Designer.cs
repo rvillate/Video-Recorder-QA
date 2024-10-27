e2a0f7714d1b304dfeb3cba70f5a11fd124dcc4d
DESKTOP-Q86BB0O\DevraYa <raulvillate15@gmail.com>
2024-10-26 21:53:04 -0500

    Agregar archivos de proyecto.

---
+++ Video Recorder QA/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Video_Recorder_QA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_inicia_grabacion = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ruta_grabacion = new System.Windows.Forms.TextBox();
+            this.chk_memoria_llena = new System.Windows.Forms.CheckBox();
+            this.chk_audio_mic = new System.Windows.Forms.CheckBox();
+            this.chk_audio_sistema = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.espacio_libre = new System.Windows.Forms.Label();
+            this.txt_total_tiempo_grabado = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_inicia_grabacion
+            // 
+            this.btn_inicia_grabacion.Location = new System.Drawing.Point(285, 16);
+            this.btn_inicia_grabacion.Name = "btn_inicia_grabacion";
+            this.btn_inicia_grabacion.Size = new System.Drawing.Size(123, 23);
+            this.btn_inicia_grabacion.TabIndex = 0;
+            this.btn_inicia_grabacion.Text = "Iniciar grabación";
+            this.btn_inicia_grabacion.UseVisualStyleBackColor = true;
+            this.btn_inicia_grabacion.Click += new System.EventHandler(this.btn_inicia_grabacion_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_ruta_grabacion);
+            this.groupBox1.Controls.Add(this.chk_memoria_llena);
+            this.groupBox1.Controls.Add(this.chk_audio_mic);
+            this.groupBox1.Controls.Add(this.chk_audio_sistema);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_inicia_grabacion);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 153);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ruta de grabación:";
+            // 
+            // txt_ruta_grabacion
+            // 
+            this.txt_ruta_grabacion.Location = new System.Drawing.Point(130, 124);
+            this.txt_ruta_grabacion.Name = "txt_ruta_grabacion";
+            this.txt_ruta_grabacion.Size = new System.Drawing.Size(245, 23);
+            this.txt_ruta_grabacion.TabIndex = 11;
+            this.txt_ruta_grabacion.Text = "C:\\Users\\DevraYa\\Videos\\Grabaciones";
+            // 
+            // chk_memoria_llena
+            // 
+            this.chk_memoria_llena.AutoSize = true;
+            this.chk_memoria_llena.Checked = true;
+            this.chk_memoria_llena.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_memoria_llena.Location = new System.Drawing.Point(20, 31);
+            this.chk_memoria_llena.Name = "chk_memoria_llena";
+            this.chk_memoria_llena.Size = new System.Drawing.Size(137, 19);
+            this.chk_memoria_llena.TabIndex = 10;
+            this.chk_memoria_llena.Text = "Avisar memoria llena";
+            this.chk_memoria_llena.UseVisualStyleBackColor = true;
+            // 
+            // chk_audio_mic
+            // 
+            this.chk_audio_mic.AutoSize = true;
+            this.chk_audio_mic.Location = new System.Drawing.Point(20, 53);
+            this.chk_audio_mic.Name = "chk_audio_mic";
+            this.chk_audio_mic.Size = new System.Drawing.Size(117, 19);
+            this.chk_audio_mic.TabIndex = 9;
+            this.chk_audio_mic.Text = "Audio microfono";
+            this.chk_audio_mic.UseVisualStyleBackColor = true;
+            // 
+            // chk_audio_sistema
+            // 
+            this.chk_audio_sistema.AutoSize = true;
+            this.chk_audio_sistema.Location = new System.Drawing.Point(20, 74);
+            this.chk_audio_sistema.Name = "chk_audio_sistema";
+            this.chk_audio_sistema.Size = new System.Drawing.Size(101, 19);
+            this.chk_audio_sistema.TabIndex = 8;
+            this.chk_audio_sistema.Text = "Audio sistema";
+            this.chk_audio_sistema.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Extraer ultimos 0 segundos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Grabar ultimos (s)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(130, 96);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.espacio_libre);
+            this.groupBox2.Controls.Add(this.txt_total_tiempo_grabado);
+            this.groupBox2.Location = new System.Drawing.Point(12, 171);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 109);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Estado";
+            // 
+            // espacio_libre
+            // 
+            this.espacio_libre.AutoSize = true;
+            this.espacio_libre.Location = new System.Drawing.Point(20, 32);
+            this.espacio_libre.Name = "espacio_libre";
+            this.espacio_libre.Size = new System.Drawing.Size(126, 15);
+            this.espacio_libre.TabIndex = 7;
+            this.espacio_libre.Text = "Espacio libre en disco: ";
+            // 
+            // txt_total_tiempo_grabado
+            // 
+            this.txt_total_tiempo_grabado.AutoSize = true;
+            this.txt_total_tiempo_grabado.Location = new System.Drawing.Point(20, 14);
+            this.txt_total_tiempo_grabado.Name = "txt_total_tiempo_grabado";
+            this.txt_total_tiempo_grabado.Size = new System.Drawing.Size(127, 15);
+            this.txt_total_tiempo_grabado.TabIndex = 5;
+            this.txt_total_tiempo_grabado.Text = "Tiempo total grabado: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(432, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(311, 268);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 292);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "QA - Grabación pantalla";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private Button btn_inicia_grabacion;
+        private GroupBox groupBox1;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private GroupBox groupBox2;
+        private Label txt_total_tiempo_grabado;
+        private Label espacio_libre;
+        private Button button2;
+        private CheckBox chk_audio_mic;
+        private CheckBox chk_audio_sistema;
+        private CheckBox chk_memoria_llena;
+        private Label label3;
+        private TextBox txt_ruta_grabacion;
+        private Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ListView listView1;
+    }
+}
---
+++ Video Recorder QA/Form1.cs
@@ -0,0 +1,175 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using ScreenRecorderLib;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Management;
+
+namespace Video_Recorder_QA
+{
+    public partial class Form1 : Form
+    {
+        private Recorder _rec;
+        private int _sequenceId = 1;
+        private System.Timers.Timer _timer;
+        private bool _isRecording = false;
+
+        int espacioEnGB;
+        int espacioEnMB;
+
+        private TimeSpan _totalTiempoGrabado = TimeSpan.Zero;
+
+        private string rutaGrabacion = "C:\\Users\\DevraYa\\Videos\\Grabaciones";
+
+        private System.Timers.Timer _tiempoGrabadoTimer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_inicia_grabacion_Click(object sender, EventArgs e)
+        {
+            if (btn_inicia_grabacion.Text.Equals("Iniciar grabación"))
+            {
+                ObtenerEspacioLibreEnMB(txt_ruta_grabacion.Text);
+                if (espacioEnGB * 1024 + espacioEnMB < 512)
+                {
+                    DialogResult result = MessageBox.Show(@"Poco espacio en disco C:\. Este disco se usa para guardar un archivo temporal de grabación, ¿seguro que desea continuar?", "Poco espacio en disco", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+
+                // Iniciar la grabación
+                CreateRecording();
+                btn_inicia_grabacion.Text = "Detener grabación";
+            }
+            else if (btn_inicia_grabacion.Text == "Detener grabación")
+            {
+                // Detener la grabación cuando se detenga manualmente
+                await EndRecordingAsync();
+                btn_inicia_grabacion.Text = "Iniciar grabación";
+            }
+        }
+
+        private void ObtenerEspacioLibreEnMB(string ruta)
+        {
+            string root = Path.GetPathRoot(ruta);
+
+            DriveInfo driveInfo = new DriveInfo(root);
+
+            long freeSpaceBytes = driveInfo.AvailableFreeSpace;
+
+            // Convertir el espacio libre a MB
+            int freeSpaceMB = (int)(freeSpaceBytes / (1024 * 1024));
+
+            espacioEnGB = Convert.ToInt32(freeSpaceMB / 1024);
+            espacioEnMB = Math.Abs((espacioEnGB * 1024) - freeSpaceMB);
+            espacio_libre.Text = $"Espacio libre en disco {espacioEnGB} GB - {espacioEnMB} MB";
+
+        }
+       
+        void CreateRecording()
+        {
+            _isRecording = true;
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string videoFilePath = Path.Combine(txt_ruta_grabacion.Text, $"{timestamp}_seq{_sequenceId}.mp4"); // Ruta del archivo con el nombre dinámico
+
+            _rec = Recorder.CreateRecorder(new RecorderOptions
+            {
+                AudioOptions = new AudioOptions
+                {
+                    IsAudioEnabled = chk_audio_mic.Checked == true || chk_audio_sistema.Checked == true ? true : false,
+                    IsInputDeviceEnabled = chk_audio_mic.Checked,
+                    IsOutputDeviceEnabled = chk_audio_sistema.Checked
+                }
+            });
+            //_rec.OnRecordingComplete += Rec_OnRecordingComplete;
+            _rec.OnRecordingFailed += Rec_OnRecordingFailed;
+            _rec.OnStatusChanged += Rec_OnStatusChanged;
+
+            _rec.Record(videoFilePath);
+
+            _sequenceId++;
+
+        }
+
+        async Task EndRecordingAsync()
+        {
+            if (_rec != null && _isRecording)
+            {
+                await Task.Run(() => _rec.Stop());
+                _isRecording = false;
+            }
+        }
+
+        private void Rec_OnRecordingFailed(object sender, RecordingFailedEventArgs e)
+        {
+            string error = e.Error;
+            Console.WriteLine($"Error en la grabación: {error}");
+        }
+
+        private void Rec_OnStatusChanged(object sender, RecordingStatusEventArgs e)
+        {
+            RecorderStatus status = e.Status;
+            Console.WriteLine($"Estado de la grabación: {status}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            txt_ruta_grabacion.Text = rutaGrabacion;
+        }
+
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ObtenerEspacioLibreEnMB(txt_ruta_grabacion.Text);
+
+            if (chk_memoria_llena.Checked == true)
+            {
+
+            }
+
+        }
+
+        private async void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_isRecording)
+            {
+                // Detener la grabación actual
+                await EndRecordingAsync();
+
+                // Pausar brevemente para asegurar que el archivo se guarda correctamente
+                await Task.Delay(1000);
+            }
+        }
+
+        private async void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_isRecording)
+            {
+                // Detener la grabación actual
+                await EndRecordingAsync();
+
+                // Pausar brevemente para asegurar que el archivo se guarda correctamente
+                await Task.Delay(1000);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txt_ruta_grabacion.Text = folder.SelectedPath;
+            }
+        }
+    }
+}
